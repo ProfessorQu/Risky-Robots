2b30dc87270b1f7f676f28df0c1692bfee0ac485
ProfessorQu <quint@dasookwat.com>
2021-11-27 15:31:30 +0100

    Renamed some variables/ added some new ones, Made a platform class

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import pygame
-from src.player import Player
-from src.object import Object
+from pygame.locals import *
+
+from src.player import Player, Platform
+
 pygame.init()
 
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
 running = True
 
-screen = pygame.display.set_mode((800, 600))
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+CLOCK = pygame.time.Clock()
 
-p = Player(pygame.Rect(100, 100, 50, 50), (255, 0, 0))
-o = Object(pygame.Rect(200, 200, 50, 50), (0, 255, 0))
+player = Player(
+    pygame.Rect(100, 100, 50, 50),
+    (255, 0, 0)
+)
+
+platform = Platform(
+    pygame.Rect(0, HEIGHT - 20, WIDTH, 20),
+    (0, 255, 0)
+)
 
 while running:
-    screen.fill((255, 255, 255))
+    SCREEN.fill((255, 255, 255))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    player.update()
+    player.draw(SCREEN)
+    platform.update()
+    platform.draw(SCREEN)
     pygame.display.flip()
+
+    CLOCK.tick(FPS)
--- src/player.py
+++ src/player.py
@@ -1,23 +1,34 @@
 import pygame
 
 
-class Player:
-    def __init__(self, rect: pygame.Rect, color):
+# The 2D platformer player class.
+class Player(pygame.sprite.Sprite):
+    def __init__(self, rect, color):
+        super().__init__()
+
+        self.image = pygame.Surface(rect.size)
+        self.image.fill(color)
+
         self.rect = rect
-        self.color = color
-
-        self.speed = 1
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
 
     def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += self.speed
-        if keys[pygame.K_UP]:
-            self.rect.y -= self.speed
-        if keys[pygame.K_DOWN]:
-            self.rect.y += self.speed
+        pass
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, rect, color):
+        super().__init__()
+
+        self.image = pygame.Surface(rect.size)
+        self.image.fill(color)
+
+        self.rect = rect
+
+    def update(self):
+        pass
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
