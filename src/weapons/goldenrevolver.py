f24c05c7f7ec80d9ca384fc6423ca34d5d5e84ab
ProfessorQu <quint@dasookwat.com>
2021-12-17 19:40:35 +0100

    Added the golden revolver.

---
+++ src/weapons/goldenrevolver.py
@@ -0,0 +1,18 @@
+import pygame
+
+from src.weapons.data import *
+
+
+BULLET = BulletData(
+    image = pygame.image.load("src/assets/bullets/goldenbullet.png"),
+    size = (25, 25),
+    speed = 500,
+    damage = 100
+)
+
+WEAPON = WeaponData(
+    image = pygame.image.load("src/assets/weapons/goldenrevolver.png"),
+    size = (50, 50),
+    cooldown = 100,
+    bullet = BULLET
+)
