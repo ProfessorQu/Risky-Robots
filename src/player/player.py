2209cd4d5d86bdf07f925ed39e632d9710b89d7f
ProfessorQu <quint@dasookwat.com>
2021-12-17 19:01:21 +0100

    Split the update function into smaller functions.

--- src/player/player.py
+++ src/player/player.py
@@ -8,7 +8,7 @@ from src.player.inputs import Inputs
 from src.terrain import Terrain
 from src.weapons import revolver
 
-from typing import Tuple
+from typing import List, Tuple
 import os
 import random
 
@@ -60,7 +60,7 @@ class Player(pygame.sprite.Sprite):
         self.hurt = pygame.image.load(f"src/assets/player{player_id}/hurt.png").convert_alpha()
         self.hurt = pygame.transform.scale(self.hurt, (self.hurt.get_width() * player.SCALE, self.hurt.get_height() * player.SCALE))
 
-        self.hurt_time = 0
+        self.hurt_image_time = 0
 
         # Animation variables
         self.animation_speed = random.uniform(player.ANIMATION_SPEED_MIN, player.ANIMATION_SPEED_MAX)
@@ -82,7 +82,7 @@ class Player(pygame.sprite.Sprite):
         # Movement
         self.velocity = pygame.math.Vector2(0, 0)
         self.facing_right = facing_right
-        self.ground_timer = 0
+        self.grounded_timer = 0
 
         self.jump_time_counter = 0
         self.is_jumping = False
@@ -106,22 +106,43 @@ class Player(pygame.sprite.Sprite):
         self.health -= damage
         
         # Set the hurt time
-        self.hurt_time = player.HURT_TIME
+        self.hurt_image_time = player.HURT_TIME
 
         self.knockback_dir = direction
 
-    def handle_movement(self, dt: float, inputs: list):
-        """Handle the movement of the player
+    def update_inputs(self, dt: float) -> Bullet:
+        """Update the inputs of the player
 
         Args:
-            dt (float): the time since the last update
-            inputs (list): the inputs of the player
+            dt (float): the time since the last frame
+
+        Returns:
+            Bullet: the bullet that was shot
         """
-        # Set variables
-        jump_input = Direction.UP in inputs
+        # Get inputs
+        inputs = self.inputs.get_inputs()
+
+        # Horizontal movement
+        self.horizontal_move(dt, inputs)
+
+        # Jump
+        self.jump(dt, inputs)
+
+        # Limit the velocity
+        self.limit_speed()
+
+        # Check if the player shot a bullet
+        return self.check_shot(inputs)
+
+    def horizontal_move(self, dt: float, inputs: List):
+        """Move the player horizontally
+
+        Args:
+            dt (float): the time since the last frame
+            inputs (List): the inputs of the player
+        """
         left_input = Direction.LEFT in inputs
         right_input = Direction.RIGHT in inputs
-
         # Move left
         if left_input:
             self.velocity.x -= player.ACCELERATION * dt
@@ -134,46 +155,94 @@ class Player(pygame.sprite.Sprite):
             self.facing_right = True
 
             self.state = "walking"
-
-        # Jump
-        if jump_input and self.ground_timer > 0:
-            self.is_jumping = True
-            self.jump_time_counter = player.JUMP_TIME
-            self.velocity.y = player.JUMP_HEIGHT * dt
-
-        if jump_input and self.is_jumping:
-            if self.jump_time_counter > 0:
-                self.velocity.y = player.JUMP_HEIGHT * dt
-                self.jump_time_counter -= dt
-            else:
-                self.is_jumping = False
-        
-        if not jump_input:
-            self.is_jumping = False
-
+            
         # If no movement, decelerate
         if not left_input and not right_input:
             self.velocity.x -= self.velocity.x * player.FRICTION * dt
 
             if abs(self.velocity.x) < 1:
                 self.velocity.x = 0
-                
+
             self.state = "idle"
+
+    def jump(self, dt: float, inputs: List):
+        """Check if the player should jump
+
+        Args:
+            dt (float): the time since the last frame
+            inputs (List): the inputs of the player
+        """
+        # Get the jump input
+        jump_input = Direction.UP in inputs
+        # Check if player can jump
+        if jump_input and self.grounded_timer > 0:
+            self.is_jumping = True
+            self.jump_time_counter = player.JUMP_TIME
+            self.velocity.y = player.JUMP_HEIGHT * dt
+
+        # Check if the player is jumping
+        if jump_input and self.is_jumping:
+            if self.jump_time_counter > 0:
+                self.velocity.y = player.JUMP_HEIGHT * dt
+                self.jump_time_counter -= dt
+            else:
+                self.is_jumping = False
+
+        # Check if the player is not jumping
+        if not jump_input:
+            self.is_jumping = False
         
+        # Decrease the grounded timer
+        self.grounded_timer -= dt
+
+    def limit_speed(self):
+        """Limit the speed of the player
+        """
         # Limit the speed
-        self.velocity.x = min(self.velocity.x, player.MAX_SPEED)
-        self.velocity.x = max(self.velocity.x, -player.MAX_SPEED)
+        if abs(self.velocity.x) > player.MAX_SPEED:
+            self.velocity.x = player.MAX_SPEED * self.velocity.x / abs(self.velocity.x)
 
         # Check if the player is on the ground
         if self.velocity.y >= 1:
             self.state = "falling"
         elif self.velocity.y < 0:
             self.state = "jumping"
-        
-        self.ground_timer -= dt
 
-    def handle_collisions(self):
-        """Handle the collisions of the player
+    def check_shot(self, inputs: List) -> Bullet:
+        """Check if the player shot a bullet
+
+        Args:
+            inputs (List): the inputs of the player
+
+        Returns:
+            Bullet: the bullet that was shot
+        """
+        return self.weapon.shoot() if Direction.DOWN in inputs else None
+
+    def knockback(self, dt: float):
+        """Apply knockback to the player
+
+        Args:
+            dt (float): the time since the last frame
+        """
+        # Apply knockback
+        if self.knockback_dir != 0:
+            self.velocity.x = self.knockback_dir * player.HORIZONTAL_KNOCKBACK * dt
+            self.velocity.y = player.VERTICAL_KNOCKBACK * dt
+            self.knockback_dir = 0
+    
+    def gravity(self, dt: float):
+        """Apply gravity to the player
+
+        Args:
+            dt (float): the time since the last frame
+        """
+        # Gravity
+        gravity = player.GRAVITY * dt if self.velocity.y < 0 else player.GRAVITY_FALL * dt
+        self.velocity.y += gravity
+    
+    def collide(self):
+        """Check if the player collided with the terrain
         """
         # Get all collisions
         collisions = self.terrain.collide(self)
@@ -181,6 +250,7 @@ class Player(pygame.sprite.Sprite):
         # Check if grounded
         grounded = False
 
+        # Go through all collisions
         for (tile, direction) in collisions:
             if direction == Direction.UP:
                 self.velocity.y = tile.rect.bottom - self.rect.top
@@ -193,14 +263,30 @@ class Player(pygame.sprite.Sprite):
             if direction == Direction.RIGHT:
                 self.velocity.x = tile.rect.left - self.rect.right
         
+        # Set the grounded timer if the player is grounded
         if grounded:
-            self.ground_timer = player.HANG_TIME
+            self.grounded_timer = player.HANG_TIME
+
+    def update_other(self):
+        """Update the weapon and the healthbar
+        """
+        # Update the weapon
+        direction = 1 if self.facing_right else -1
+        weapon_x = self.rect.centerx + (direction * self.image.get_width() * 0.5)
+
+        self.weapon.update(
+            (weapon_x, self.rect.centery),
+            direction
+        )
+
+        # Update the healthbar
+        self.healthbar.update(self.rect.center)
 
     def update_animation(self, dt: float):
         """Update the animation of the player
 
         Args:
-            dt (float): the time since the last update
+            dt (float): the time since the last frame
         """
         # Update the animation
         self.update_time += dt
@@ -215,15 +301,24 @@ class Player(pygame.sprite.Sprite):
             self.update_time = 0
             self.animation_speed = random.uniform(player.ANIMATION_SPEED_MIN, player.ANIMATION_SPEED_MAX)
         
-        # Set the image
+        # Set image if falling or jumping
         if self.state == "falling":
             self.image = self.falling
         elif self.state == "jumping":
             self.image = self.jumping
         
-        if self.hurt_time > 0:
+        # Set image if hurt
+        if self.hurt_image_time > 0:
             self.image = self.hurt
-            self.hurt_time -= dt
+            self.hurt_image_time -= dt
+
+    def check_health(self):
+        """Check if the player is dead
+        """
+        if self.health <= 0:
+            print(f"Player {self.player_id} died!")
+            print(f"Player {1 if self.player_id == 2 else 2} won!")
+            pygame.event.post(pygame.event.Event(pygame.QUIT))
 
     def update(self, dt: float) -> Bullet:
         """Update the player
@@ -234,48 +329,29 @@ class Player(pygame.sprite.Sprite):
         Returns:
             Bullet: the new bullet
         """
-        # Get inputs
-        inputs = self.inputs.get_inputs()
-
-        # Handle inputs
-        self.handle_movement(dt, inputs)
-        new_bullet = self.weapon.shoot() if Direction.DOWN in inputs else None
+        new_bullet = self.update_inputs(dt)
 
         # Apply knockback
-        if self.knockback_dir != 0:
-            self.velocity.x = self.knockback_dir * player.HORIZONTAL_KNOCKBACK * dt
-            self.velocity.y = player.VERTICAL_KNOCKBACK * dt
-            self.knockback_dir = 0
+        self.knockback(dt)
 
-        # Gravity
-        gravity = player.GRAVITY * dt if self.velocity.y < 0 else player.GRAVITY_FALL * dt
-        self.velocity.y += gravity
+        # Apply gravity
+        self.gravity(dt)
 
         # Handle collisions
-        self.handle_collisions()
+        self.collide()
 
-        # Move the player
-        self.rect.x += self.velocity.x
-        self.rect.y += self.velocity.y
-
-        # Update the weapon
-        direction = 1 if self.facing_right else -1
-        weapon_x = self.rect.centerx + (direction * self.image.get_width() * 0.5)
-
-        self.weapon.update(
-            (weapon_x, self.rect.centery),
-            direction
-        )
-
-        # Update the healthbar
-        self.healthbar.update(self.rect.center)
+        # Update animation
         self.update_animation(dt)
 
-        # Check if the player is dead
-        if self.health <= 0:
-            print(f"Player {self.player_id} died!")
-            print(f"Player {1 if self.player_id == 2 else 2} won!")
-            pygame.event.post(pygame.event.Event(pygame.QUIT))
+        # Check health
+        self.check_health()
+
+        # Update the player's position
+        self.rect.x += self.velocity.x
+        self.rect.y += self.velocity.y
+        
+        # Update weapon and healthbar
+        self.update_other()
 
         return new_bullet
 
@@ -294,7 +370,3 @@ class Player(pygame.sprite.Sprite):
 
         # Draw the healthbar
         self.healthbar.draw(screen, self.health)
-
-
-
-
