1a06e8b45764438b864872a045feb67942740f49
ProfessorQu <quint@dasookwat.com>
2021-12-21 12:55:24 +0100

    Added bounds to spread shoot.

--- src/weapons/functions/shoot.py
+++ src/weapons/functions/shoot.py
@@ -22,9 +22,9 @@ def spread(weapon):
         weapon.rect.centerx + (weapon.dir * weapon.image.get_width() / 2),
         weapon.rect.centery - weapon.image.get_height() / 4
     )
-    bullets.append(Bullet(weapon.weapon_type.bullet, bullet_pos, (weapon.dir, 0), weapon.terrain))
-    bullets.append(Bullet(weapon.weapon_type.bullet, bullet_pos, (weapon.dir, -0.2), weapon.terrain))
-    bullets.append(Bullet(weapon.weapon_type.bullet, bullet_pos, (weapon.dir, 0.2), weapon.terrain))
+    bullets.append(Bullet(weapon.weapon_type.bullet, bullet_pos, (weapon.dir, 0), weapon.bounds, weapon.terrain))
+    bullets.append(Bullet(weapon.weapon_type.bullet, bullet_pos, (weapon.dir, -0.2), weapon.bounds, weapon.terrain))
+    bullets.append(Bullet(weapon.weapon_type.bullet, bullet_pos, (weapon.dir, 0.2), weapon.bounds, weapon.terrain))
     # Reset fire rate
     weapon.cooldown = weapon.weapon_type.cooldown
 
